15c39fcd56a63f98152fac2d4d5e75ec095aac0f
JBrozinski <joseph.brozinski@gmail.com>
2020-02-11 15:30:40 -0700

    Added psuedocode to js

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// create an array with words to be guessed.
+
+
+// pick a word randomly
+
+
+// Set up the answer
+
+
+// Game Play Loop:
+// Display blank spaces from Mystery Word
+// Prompt the player to guess a letter
+// Update the game with the player's guess
+//
+
